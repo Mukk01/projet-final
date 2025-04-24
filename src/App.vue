24c02c36ce20fa66ce36e7e45d326ccdccf49999
Mukk <charlespg2005@icloud.com>
2025-04-24 19:26:59 -0400

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,6 @@
+<template>
+  <div class="p-4">
+    <h1 class="text-3xl font-bold text-center text-primary">Hello from Vue 3 + DaisyUI!</h1>
+    <button class="btn btn-secondary mt-5">Click Me</button>
+  </div>
+</template>
